541815501e0a2372b14f93f389c5415d4fa246ee
vishwesh5 <vishweshshrimali5@gmail.com>
2018-11-02 13:39:37 +0530

    Add adaline codes

--- iris.py
+++ iris.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from perceptron import *
 from util import *
 
 # Load data from UCI ML Repo
@@ -59,3 +58,22 @@ for n_iter in range(1,11):
     plt.title('Number of iterations = {}'.format(n_iter))
     plt.legend(loc='upper left')
     plt.show()
+
+# Adaline GD
+fig,ax = plt.subplots(nrows=1,ncols=2,figsize=(10,4))
+
+ada1 = AdalineGD(n_iter=10,eta=0.01).fit(X,y)
+ax[0].plot(range(1,len(ada1.cost_)+1),
+        np.log10(ada1.cost_),marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-Squared-Error)')
+ax[0].set_title('Adaline - Learning Rate 0.01')
+
+ada2 = AdalineGD(n_iter=100,eta=0.0001).fit(X,y)
+ax[1].plot(range(1,len(ada2.cost_)+1),
+        np.log10(ada2.cost_),marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('log(Sum-Squared-Error)')
+ax[1].set_title('Adaline - Learning Rate 0.0001')
+
+plt.show()
--- util.py
+++ util.py
@@ -3,6 +3,177 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Perceptron class
+
+class Perceptron(object):
+    """Perceptron classifier.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    errors_ : list
+      Number of misclassifications (updates) in each epoch.
+
+    """
+    def __init__(self, eta=0.01,
+            n_iter=50, random_state=1):
+        # Scalar
+        self.eta = eta
+        # Scalar
+        self.n_iter = n_iter
+        # Scalar
+        self.random_state = random_state
+        #print("[INFO] Initialized parameters")
+
+    def fit(self, X, y):
+        """Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+          Training vectors, where n_samples is the number of
+          samples and
+          n_features is the number of features.
+        y : array-like, shape = [n_samples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        #print("[INFO] Fitting values")
+        rgen = np.random.RandomState(self.random_state)
+        # w_ Shape: (1+n_features)
+        # w_[0] = bias
+        # X Shape: (n_samples, n_features)
+        # y Shape: (n_samples)
+        self.w_ = rgen.normal(loc=0.0,scale=0.01,
+                size=1+X.shape[1])
+        self.errors_ = []
+
+        for i in range(self.n_iter):
+            errors = 0
+            for xi,target in zip(X,y):
+                update = self.eta * (target-self.predict(xi))
+                self.w_[1:] += update*xi
+                # bias
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self,X):
+        """Calculate net input"""
+        #print("[INFO] Calculating net input")
+        # (n_samples, n_features).(n_features)
+        # -> (n_samples)
+        # Returns: n_samples
+        # wT X + b
+        return np.dot(X, self.w_[1:])+self.w_[0]
+
+    def predict(self,X):
+        """Return class label after unit step"""
+        #print("[INFO] Predicting class label after unit step")
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+# Adaline Class with Gradient Descent
+
+class AdalineGD(object):
+    """ADAptive LInear NEuron classifier.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    cost_ : list
+      Sum-of-squares cost function value in each epoch.
+
+    """
+    def __init__(self, eta=0.01,
+            n_iter=50,
+            random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+          Training vectors, where n_samples is the number of
+          samples and
+          n_features is the number of features.
+        y : array-like, shape = [n_samples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        # Shapes:
+        # w_ -> (1+n_features); w_[0] = bias
+        # X  -> (n_samples,n_features)
+        # y  -> (n_samples)
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0,scale=0.01,
+                size = 1+X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            # net_input -> (n_samples)
+            net_input = self.net_input(X)
+            # output -> (n_samples)
+            output = self.activation(net_input)
+            # errors -> (n_samples)
+            errors = (y-output)
+            # X.T -> (n_features,n_samples)
+            # X.T.dot(errors) -> (n_features)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            # errors.sum() -> 1
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self,X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:])+self.w_[0]
+
+    def activation(self,X):
+        """Compute linear activation"""
+        return X
+
+    def predict(self,X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X))>= 0.0,
+                1, -1)
+
 def plot_decision_regions(X,y,classifier, resolution=0.02):
 
     # Setup marker generator and color map
